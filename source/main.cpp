ae4938c9c127e317f2ad5b3ef830d34b1beb5e73
agat48 <agat.paciorek@gmail.com>
2015-01-18 10:34:09 +0100

    Doc up

--- source/main.cpp
+++ source/main.cpp
@@ -9,7 +9,8 @@ using namespace cv;
 using namespace std;
 
 
-#define BUFFER_SIZE 1900
+#define BUFFER_SIZE 30
+#define OMITTED_FRAMES 6915
 
 int countDir(const char* path);
 static void drawOptFlowMap(const Mat& flow, Mat& cflowmap, int step,
@@ -33,8 +34,8 @@ int main( int argc, const char** argv )
     path = absPath + "/samples/dynamicBackground/dynamicBackground/";
     const string sampleNames[6] = {"boats","canoe","fall","fountain01","fountain02","overpass"};
     path = path + sampleNames[0] + "/input/";
-//    FTSG(path);
-    CwisarDH(path);
+    FTSG(path);
+//    CwisarDH(path);
     return 0;
 }
 
@@ -141,13 +142,8 @@ int FTSG(string path) {
     Mat frame, nextFrame, flow;
     vector<Mat> frameChannels, nextFrameChannels, flowChannels, tensorChannels;
     vector<Mat> flowBuffer[3]; //flow history for each channel
-    BackgroundSubtractorMOG2 mog(BUFFER_SIZE, 16, false);
-    namedWindow("oryg", CV_WINDOW_AUTOSIZE); //create a window with the name "Flux Tensor"
-    namedWindow("Flux Tensor", CV_WINDOW_AUTOSIZE); //create a window with the name "Flux Tensor"
-//    setMouseCallback( "Flux Tensor", onMouse, 0 );
-    namedWindow("Split Gaussian", CV_WINDOW_AUTOSIZE); //create a window with the name "Flux Tensor"
-    namedWindow("output", CV_WINDOW_AUTOSIZE);
-    sprintf(filename, "in%06d.jpg", 1);
+    BackgroundSubtractorMOG2 mog(3, 16, false);
+    sprintf(filename, "in%06d.jpg", OMITTED_FRAMES + 1);
     frame = imread(path + filename, CV_LOAD_IMAGE_UNCHANGED);
     vector<Mat> average;
     Mat tensor(frame.rows,frame.cols, CV_32FC1,0.0);//, tensorMerge;
@@ -160,7 +156,7 @@ int FTSG(string path) {
         flowChannels.emplace_back();
         average.emplace_back(frame.rows,frame.cols, CV_32FC2,0.0);
     }
-    for (int i = 1; i<sampleCount-1; i++) { //loop over frames
+    for (int i = OMITTED_FRAMES + 1; i < BUFFER_SIZE + OMITTED_FRAMES; i++) { //loop over frames
         sprintf(filename, "in%06d.jpg", i+1);
         nextFrame = imread(path + filename, CV_LOAD_IMAGE_UNCHANGED);
 
@@ -172,37 +168,69 @@ int FTSG(string path) {
         }
         split(frame, frameChannels);
         split(nextFrame, nextFrameChannels);
-        for(int i = 0; i < frameChannels.size(); i++) {
-            calcOpticalFlowFarneback(frameChannels[i], nextFrameChannels[i], flowChannels[i], 0.5, 3, 15, 3, 5, 1.2, 0);
+        for(int k = 0; k < 3; k++) {
+            calcOpticalFlowFarneback(frameChannels[k], nextFrameChannels[k], flowChannels[k], 0.5, 3, 15, 3, 5, 1.2, 0);
         }
 //        drawOptFlowMap(flow[0], frame, 16, 1.5, Scalar(0, 255, 0));
 //        split(flow, channels); //split image into separate channels
 //        imshow("Flux Tensor", cflow); //display the image which is stored in the 'img' in the "Flux Tensor" window
 
 //        waitKey(0); //wait infinite time for a keypress
-        if(flowBuffer[0].size() >= BUFFER_SIZE) {
-            for (int i = 0; i < 3; i++) {
-                flowBuffer[i].erase(flowBuffer[i].begin());
+        for (int k = 0; k < 3; k++) {
+            flowBuffer[k].push_back(flowChannels[k]);
+        }
+//        calculateAverage(flowBuffer, average);
+//        calculateVariance(flowBuffer, average, tensor);
+////        for(int i = 0; i < 3; i++) {
+////            calcCovarMatrix(flowBuffer[i], tensorChannels[i], average[i], 0);
+////        }
+////        minMaxLoc(tensor, &min, &max); //minimalna i maksymalna wartość
+////        cout << min << " " << max << endl;
+        mog(frame,bgMask,0.004);
+////        merge(tensorChannels, tensorMerge);
+////        cvtColor(tensorMerge, tensorBin, COLOR_BGR2GRAY);
+//        threshold(tensor, tensorBin, 0.09, 1, THRESH_BINARY);
+//        threshold(bgMask, bgMaskBin, 150, 1, THRESH_BINARY);
+////        tensor.copyTo(window);
+//        bgMaskBin.convertTo(bgMaskBin, tensorBin.type());
+////        moving = tensorBin & bgMaskMerge;
+//        bitwise_and(tensorBin, bgMaskBin, moving); //moving object detection
+//        bitwise_xor(tensorBin, bgMaskBin, amb); //ambiguous regions detection
+//        amb.convertTo(amb, tensorBin.type());
+//        bitwise_and(amb, bgMaskBin, amb);
+        swap(frame, nextFrame);
+        cout << endl << i << endl;
+    }
+
+    namedWindow("oryg", CV_WINDOW_AUTOSIZE); //create a window with the name "Flux Tensor"
+    namedWindow("Flux Tensor", CV_WINDOW_AUTOSIZE); //create a window with the name "Flux Tensor"
+    namedWindow("Split Gaussian", CV_WINDOW_AUTOSIZE); //create a window with the name "Flux Tensor"
+    namedWindow("output", CV_WINDOW_AUTOSIZE);
+
+    for (int i = BUFFER_SIZE + OMITTED_FRAMES; i < sampleCount-1; i++) { //loop over frames
+        sprintf(filename, "in%06d.jpg", i+1);
+        nextFrame = imread(path + filename, CV_LOAD_IMAGE_UNCHANGED);
+
+        if (frame.empty()||nextFrame.empty()) //check whether the image is loaded or not
+        {
+            cout << "Error : Image cannot be loaded..!!" << endl;
+            return -1;
+        }
+        split(frame, frameChannels);
+        split(nextFrame, nextFrameChannels);
+        for(int k = 0; k < 3; k++) {
+            calcOpticalFlowFarneback(frameChannels[k], nextFrameChannels[k], flowChannels[k], 0.5, 3, 15, 3, 5, 1.2, 0);
+        }
+            for (int k = 0; k < 3; k++) {
+                flowBuffer[k].erase(flowBuffer[k].begin());
+                flowBuffer[k].push_back(flowChannels[k]);
             }
-        }
-        for (int i = 0; i < 3; i++) {
-            flowBuffer[i].push_back(flowChannels[i]);
-        }
         calculateAverage(flowBuffer, average);
         calculateVariance(flowBuffer, average, tensor);
-//        for(int i = 0; i < 3; i++) {
-//            calcCovarMatrix(flowBuffer[i], tensorChannels[i], average[i], 0);
-//        }
-//        minMaxLoc(tensor, &min, &max); //minimalna i maksymalna wartość
-//        cout << min << " " << max << endl;
-        mog(frame,bgMask,0.02);
-//        merge(tensorChannels, tensorMerge);
-//        cvtColor(tensorMerge, tensorBin, COLOR_BGR2GRAY);
-        threshold(tensor, tensorBin, 0.09, 1, THRESH_BINARY);
+        mog(frame,bgMask,0.004);
+        threshold(tensor, tensorBin, 0.03, 1, THRESH_BINARY);
         threshold(bgMask, bgMaskBin, 150, 1, THRESH_BINARY);
-//        tensor.copyTo(window);
         bgMaskBin.convertTo(bgMaskBin, tensorBin.type());
-//        moving = tensorBin & bgMaskMerge;
         bitwise_and(tensorBin, bgMaskBin, moving); //moving object detection
         bitwise_xor(tensorBin, bgMaskBin, amb); //ambiguous regions detection
         amb.convertTo(amb, tensorBin.type());
@@ -210,15 +238,15 @@ int FTSG(string path) {
         mog.getBackgroundImage(bg);
         bgDiff = abs(frame - bg);
         cvtColor(bgDiff, fgMask, COLOR_BGR2GRAY);
-        threshold(fgMask, fgMaskBin, 10, 1, THRESH_BINARY);
+        threshold(fgMask, fgMaskBin, 2, 1, THRESH_BINARY);
         amb.convertTo(amb, fgMaskBin.type());
         bitwise_and(fgMaskBin, amb, fgMaskBin); //static foreground mask
         fgMaskBin.convertTo(fgMaskBin, moving.type());
         bitwise_or(moving, fgMaskBin, output);
         imshow("oryg", frame);
         imshow("Flux Tensor", tensorBin);
-        imshow("Split Gaussian", fgMask);
-        imshow("output", fgMaskBin);
+        imshow("Split Gaussian", moving);
+        imshow("output", output);
         waitKey(1);
         swap(frame, nextFrame);
     }
